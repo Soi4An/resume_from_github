09d8449c301a93d6beaf8ae51d6f47b633544158
Antonio S <soi4.toxa@ukr.net>
2024-06-03 18:51:32 +0300

    Add GitHub Actions workflow for deploying to GitHub Pages

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,16 @@
 import axios from 'axios';
+// import { YOUR_ACCESS_TOKEN_GITHUB } from '../config';
 
 const BASE_URL = 'https://api.github.com';
 
 const instance = axios.create({
   baseURL: BASE_URL,
+  // headers: {
+  //   'User-Agent': 'Soi4An'
+  // }
+  headers: {
+    Authorization: `Bearer ${process.env.YOUR_ACCESS_TOKEN_GITHUB}`
+  }
 });
 
 export const client = {
--- src/api/getLanguages.ts
+++ src/api/getLanguages.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { Repository } from "../types/Repository";
 import { Languages } from "../types/Languages";
+// import { YOUR_ACCESS_TOKEN_GITHUB } from "../config";
 
 export const getLanguages = (repo: Repository) => {
-  return axios.get<Languages>(repo.languages_url)
+  return axios.get<Languages>(repo.languages_url, {
+    headers: {
+      Authorization: `Bearer ${process.env.YOUR_ACCESS_TOKEN_GITHUB}`
+    }
+  })
     .then(response => response.data);
 };
